9c54b74fcfb19272ae2f352c5ab64c50a196b6aa
Mark Rose <Mark.Rose@nasa.gov>
2015-10-02 14:10:41 -0700

    Added contact and tickets pages. Made buttons on home page work. Added email obfuscation.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-/* Replace all Google Maps links with Apple links, if on iPhone or iPad. */
 window.onload = function() {
+    /* Replace all Google Maps links with Apple links, if on iPhone or iPad. */
     if ((navigator.userAgent.match(/iPhone/i))
 	|| (navigator.userAgent.match(/iPod/i))) {
 	var links = document.getElementsByTagName("a");
@@ -8,6 +8,15 @@ window.onload = function() {
 	    if (e.dataset && e.dataset.linkType=='address') {
 		e.href = e.href.replace(/maps\.google\.com/, "maps.apple.com")
 	    }
+	}
+    }
+
+    /* Remove Xs in all mailto: links. */
+    var links = document.getElementsByTagName("a");
+    for (var i=0; i < links.length; ++i) {
+	var e = links[i]
+	if (e.dataset && e.dataset.linkType=='mailto') {
+	    e.href = e.href.replace(/x/g, "")
 	}
     }
 }
